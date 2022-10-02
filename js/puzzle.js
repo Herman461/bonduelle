15aeca9966dcca77f622b8b6a00279dfbe24a144
Herman461 <zhukovskiyherman@gmail.com>
2022-10-02 15:33:27 +0300

    build: add drag and drop to puzzles

--- js/puzzle.js
+++ js/puzzle.js
@@ -45,6 +45,98 @@ const data = [
         href: null,
     },
 ]
+//
+// const dragStart = function () {
+//     this.classList.add("hold")
+// };
+//
+// const dragEnd = function () {
+//     // this.className.add("fill")
+//     this.className.remove("hold")
+// };
+//
+// const dragOver = function (e) {
+//     // Ref: https://developer.cdn.mozilla.net/en-US/docs/Web/API/Document/dragover_event
+//     e.preventDefault();
+// };
+//
+// const dragEnter = function (e) {
+//     e.preventDefault();
+//     this.className += " hovered";
+// };
+//
+// const dragLeave = function () {
+//     this.className = "empty";
+// };
+//
+// const dragDrop = function () {
+//     this.className = "empty";
+// };
+
+function dragElement(el) {
+    let pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+    el.onpointerdown = pointerDrag;
+
+    function pointerDrag(e) {
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
+        document.onpointermove = elementDrag;
+        document.onpointerup = stopElementDrag;
+    }
+
+    function elementDrag(e) {
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
+        el.style.top = el.offsetTop - pos2 + "px";
+        el.style.left = el.offsetLeft - pos1 + "px";
+
+    }
+
+    function stopElementDrag(e) {
+        document.onpointerup = null;
+        document.onpointermove = null;
+
+        const squares = document.querySelectorAll('.quiz__square')
+        for (let index = 0; index < squares.length; index++) {
+            const square = squares[index]
+
+            const squarePos = square.getBoundingClientRect()
+            const squareX = squarePos.x
+            const squareY = squarePos.y
+
+            const puzzlePos = e.target.getBoundingClientRect()
+
+            const puzzleX = puzzlePos.x
+            const puzzleY = puzzlePos.y
+            const puzzleWidth = puzzlePos.width
+            const puzzleHeight = puzzlePos.height
+
+            const x = puzzleX - squareX > 0 && puzzleX - squareX < puzzleWidth
+            const y = puzzleY - squareY > 0 && puzzleY - squareY < puzzleHeight
+
+
+            if (x && y)  {
+                el.style.top = square.offsetTop + "px";
+                el.style.left = square.offsetLeft + "px";
+                break;
+            }
+        }
+
+
+
+
+    }
+}
+
+
 
 imageObj.onload = async function() {
 
@@ -96,8 +188,15 @@ const images = []
 async function setPuzzleForm() {
 
     for (let index = 0; index < data.length; index++) {
-
         const item = data[index]
+
+        const wrapper = document.createElement('div')
+
+        wrapper.className = 'quiz__piece'
+
+        wrapper.style.width = item.width + 'px'
+        wrapper.style.height = item.height + 'px'
+
         const response = await fetch(item.imageSrc)
         const str = await response.text()
 
@@ -114,9 +213,23 @@ async function setPuzzleForm() {
         svgImage.querySelector('svg').setAttribute('viewBox', '0 0 ' + item.width + ' ' + item.height)
         svgImage.querySelector('svg').setAttribute('width', item.width)
         svgImage.querySelector('svg').setAttribute('height', item.height)
-        document.body.appendChild(svgImage.documentElement)
+        wrapper.appendChild(svgImage.documentElement)
+
+        // wrapper.setAttribute('draggable', 'true')
+        // wrapper.addEventListener("dragstart", dragStart);
+        // wrapper.addEventListener("dragend", dragEnd);
+
+        document.querySelector('.quiz__puzzles').appendChild(wrapper)
+
+        dragElement(wrapper)
         // images.push(svgImage.documentElement)
     }
-
-
 }
+
+
+//
+// const squares = document.querySelectorAll('.quiz__square')
+//
+// for (let index = 0; index < squares.length; index++) {
+//     const square = squares[index]
+// }
